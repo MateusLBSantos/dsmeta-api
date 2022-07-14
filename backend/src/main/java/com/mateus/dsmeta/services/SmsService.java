e96abcc38670a3c3597d865849b6c8b43c6d937f
MateusLBSantos <mateuslucas.ita@hotmail.com>
2022-07-14 15:34:59 -0300

    Alteração na String msg

--- backend/src/main/java/com/mateus/dsmeta/services/SmsService.java
+++ backend/src/main/java/com/mateus/dsmeta/services/SmsService.java
@@ -24,18 +24,21 @@ public class SmsService {
 
 	@Value("${twilio.phone.to}")
 	private String twilioPhoneTo;
-	
+
 	@Autowired
 	private SaleRepository repository;
 
 	public void sendSms(Long saleId) {
-		
+
 		Sale sale = repository.findById(saleId).get();
 
 		String date = sale.getDate().getMonthValue() + "/" + sale.getDate().getYear();
-		
-		String msg = "O vendedor " + sale.getSellerName() + " foi destaque em " + date 
-				+ " com o total de R$ " + String.format("%.2f", sale.getAmount());
+
+//		String msg = "O vendedor " + sale.getSellerName() + " foi destaque em " + date 
+//				+ " com o total de R$ " + String.format("%.2f", sale.getAmount());
+
+		String msg = "O vendedor " + sale.getSellerName() + " foi destaque em " + date + " com um total de R$ "
+				+ String.format("%.0f", sale.getAmount());
 
 		Twilio.init(twilioSid, twilioKey);
 
